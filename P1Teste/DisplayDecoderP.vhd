21e9bbc5d4f7d440380bfd6408ea57e4fcbc1de8
Afonso Rodrigues <afonsocr@ua.pt>
2022-06-19 16:22:56 +0100

    Fases 1/2/3/5

---
+++ P1Teste/DisplayDecoderP.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity DisplayDecoderP is
+	port( enable    : in std_logic;
+			decOut    : out std_logic_vector(6 downto 0));
+end DisplayDecoderP;
+
+architecture Behavioral of DisplayDecoderP is
+begin
+	decOut <= "0001100" when (enable = '1') else -- P
+				 "1111111";									
+end Behavioral;
--- P1Teste/P1TesteFSM.vhd
+++ P1Teste/P1TesteFSM.vhd
@@ -3,7 +3,9 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity P1TesteFSM is 
 	port( reset 	   : in  std_logic;	-- Global reset
-			P1    	   : in  std_logic;	-- Setting 2
+			P1    	   : in  std_logic;	-- Setting 1
+			P2          : in  std_logic;  -- Setting 2
+			P3          : in  std_logic;  -- Setting 3
 			startStop   : in  std_logic;	-- Start/stop
 			clk         : in  std_logic;	-- Clock
 			display1    : out std_logic;  -- Display P 
@@ -21,20 +23,30 @@ end P1TesteFSM;
 
 architecture Behavioral of P1TesteFSM is 
 	
+	constant ZERO_TIME      : std_logic_vector(7 downto 0) := "00000000"; -- 0s
 	constant P1_TIME        : std_logic_vector(7 downto 0) := "00110011"; -- 51s
+	constant P2_TIME        : std_logic_vector(7 downto 0) := "00011110"; -- 30s
+	constant P3_TIME        : std_logic_vector(7 downto 0) := "00001001"; -- 9s
 	constant meterAgua_TIME : std_logic_vector(7 downto 0) := "00000111"; -- 7s
 	constant enxaguar_TIME  : std_logic_vector(7 downto 0) := "00001010"; -- 10s
 	constant tirarAgua_TIME : std_logic_vector(7 downto 0) := "00000100"; -- 4s
 	constant	spin_TIME      : std_logic_vector(7 downto 0) := "00000101"; -- 5s
 	constant finish_TIME    : std_logic_vector(7 downto 0) := "00000010"; -- 2s  
 	
-	type TState is (TInit,TP1,TEnxaguar, TSpin,TTirarAgua, TFinish,TMeterAgua);
+	type TState is (TInit, TP1, TEnxaguar, 
+						 TSpin, TTirarAgua, TFinish, 
+						 TMeterAgua, TP2, TP3);
 						 
-	signal s_currentState, s_nextState, s_lastState : TState := TInit;
-	signal s_stateChanged : std_logic := '1';
-	signal s_ultimoestado : std_logic;
-	signal s_ultimoestado_1:std_logic;
-	signal s_ultimoestado_2:std_logic;
+	signal s_currentState, s_nextState : TState := TInit;
+	signal s_stateChanged  : std_logic := '1';
+	signal s_ultimoestado  : std_logic;
+	signal s_ultimoestado_1: std_logic;
+	signal s_ultimoestado_2: std_logic;
+	signal s_isP1          : std_logic;
+	--signal s_isMA          : std_logic;
+	--signal s_isE           : std_logic;
+	--signal s_isTA          : std_logic;
+	--signal s_isS           : std_logic;*/
 	
 
 	begin
@@ -70,9 +82,14 @@ architecture Behavioral of P1TesteFSM is
 			spin <= '0';
 			on_off <= '0';
 			timeEnable <= '0';
-			timeValue <= (others => '0');
+			timeValue <= ZERO_TIME;
+			
 			if (P1 = '1') then
 				s_nextState <= TP1;
+			elsif (P2 = '1') then
+				s_nextState <= TP2;
+			elsif (P3 = '1') then
+				s_nextState <= TP3;
 			else
 				s_nextState <= TInit;
 			end if;
@@ -90,48 +107,140 @@ architecture Behavioral of P1TesteFSM is
 				
 			if (startStop = '1') then
 				s_nextState <= TMeterAgua;
+				s_ultimoestado <= '0';
+				s_ultimoestado_1 <= '0';
+				s_ultimoestado_2 <= '0';
+				s_isP1 <= '1';
+			elsif (P1 = '0') then
+				s_nextState <= TInit;
 			else 
 				s_nextState <= TP1;
 			end if;
 -------------------------------------------------------------------
-		when Tspin =>
+		when TP2 =>
 			display1 <= '1';
-			display2 <= "1111001";
+			display2 <= "0100100";
+			water_valve <= '0';
+			rinse <= '0';
+			water_pump <= '0';
+			spin <= '0';
+			on_off <= '0';
+			timeEnable <= '0';
+			timeValue <= P2_TIME;
+			
+			if (startStop = '1') then
+				s_nextState <= TMeterAgua;
+				s_ultimoestado <= '0';
+				s_ultimoestado_1 <= '0';
+				s_ultimoestado_2 <= '0';
+				s_isP1 <= '0';
+			elsif (P2 = '0') then
+				s_nextState <= TInit;
+			else
+				s_nextState <= TP2;
+			end if;
+-------------------------------------------------------------------
+		when TP3 =>
+			display1 <= '1';
+			display2 <= "0110000";
+			water_valve <= '0';
+			rinse <= '0';
+			water_pump <= '0';
+			spin <= '0';
+			on_off <= '0';
+			timeEnable <= '0';
+			timeValue <= P3_TIME;
+		
+			if (startStop = '1') then
+				s_nextstate <= TSpin;
+			elsif (P3 = '0') then
+				s_nextState <= TInit;
+			else
+				s_nextState <= TP3;
+			end if;
+-------------------------------------------------------------------
+		--when TStop =>
+			--display1 <= '1';
+			--display2 <= "0110111";
+			--water_valve <= '0';
+			--rinse <= '0';
+			--water_pump <= '0';
+			--spin <= '0';
+			--on_off <= '0';
+			--timeEnable <= '0';
+			--timeValue <= ZERO_TIME;
+			
+			--if (startStop = '1') then
+				--if (s_isMA = '1') then
+					--s_nextState <= TMeterAgua;
+					--s_isMA <= '0';
+				--elsif (s_isE = '1') then
+					--s_nextState <= TEnxaguar;
+					--s_isE <= '0';
+				--elsif (s_isTA = '1') then
+					--s_nextState <= TTirarAgua;
+					--s_isTA <= '0';
+				--elsif (s_isS = '1') then
+					--s_nextState <= TSpin;
+					--s_isS <= '0';
+				--end if; 
+			--else
+				--s_nextState <= TStop;
+			---end if;
+-------------------------------------------------------------------
+		when TSpin =>
+			display1 <= '1';
+			display2 <= "1111111";
+			water_valve <= '0';
+			rinse <= '0';
 			water_pump <= '0';
 			spin <= '1';
 			on_off <= '1';
 			timeEnable <= '1';
 			timeValue <= spin_TIME;
 			
-			if (startStop = '1' and timeExp = '1') then
+			--if (startStop = '1') then
+				--s_nextState <= TStop;
+				--s_isS <= '1';
+			--else
+			if (timeExp = '1') then
 				s_nextstate <= TTirarAgua;
 				s_ultimoestado <= '0';
 			else
 				s_nextState <= Tspin;
 			end if;
+			--end if;
 -------------------------------------------------------------------			
 		when TTirarAgua =>
 			display1 <= '1';
-			display2 <= "1111001";
+			display2 <= "1111111";
+			water_valve <= '0';
+			rinse <= '0';
 			water_pump <= '1';
+			spin <= '0';
 			spin <= '0';
 			on_off <= '1';
 			timeEnable <= '1';
 			timeValue <= tirarAgua_TIME;
-			
-			if (startStop = '1' and timeExp = '1' and s_ultimoestado = '0') then
-				s_nextstate <= TFinish;
-			elsif (startStop = '1' and timeExp = '1' and s_ultimoestado = '1' and s_ultimoestado_1 = '1') then
+				
+			--if (startStop = '1') then
+				--s_nextState <= TStop;
+				--s_isTA <= '1';
+			--else
+			if (timeExp = '1' and s_ultimoestado = '1' and s_ultimoestado_1 = '1') then
 				s_nextstate <= TMeterAgua;
-			elsif (startStop = '1' and timeExp = '1' and s_ultimoestado = '1'and s_ultimoestado_1 = '0')then
+			elsif (timeExp = '1' and s_ultimoestado = '1'and s_ultimoestado_1 = '0') then
 				s_nextstate <= Tspin;
+			elsif (timeExp = '1' and s_ultimoestado = '0') then
+				s_nextstate <= TFinish;
 			else 
 				s_nextState <= TTirarAgua;
 			end if;
+			--end if;
 ------------------------------------------------------------------
 		when TMeterAgua =>
 			display1 <= '1';
-			display2 <= "1111001";
+			display2 <= "1111111";
 			water_valve <= '1';
 			rinse <= '0';
 			water_pump <= '0';
@@ -139,16 +248,21 @@ architecture Behavioral of P1TesteFSM is
 			on_off <= '1';
 			timeEnable <= '1';
 			timeValue <= meterAgua_TIME;
-		
-			if (timeExp = '1'and startStop = '1') then
+			
+			--if (startStop = '1') then
+				--s_nextState <= TStop;
+				--s_isMA <= '1';
+			--else
+			if (timeExp = '1') then
 				s_nextstate <= TEnxaguar;
 			else 
 				s_nextState <= TMeterAgua;
-			end if;	
+			end if;
+			--end if;
 -------------------------------------------------------------------		
 		when TEnxaguar =>
 			display1 <= '1';
-			display2 <= "1111001";
+			display2 <= "1111111";
 			water_valve <= '0';
 			rinse <= '1';
 			water_pump <= '0';
@@ -157,10 +271,18 @@ architecture Behavioral of P1TesteFSM is
 			timeEnable <= '1';
 			timeValue <= enxaguar_TIME;
 			
-			if (timeExp = '1' and startStop = '1' and s_ultimoestado_1 ='1' and s_ultimoestado_2 = '1')then
+			--if (startStop = '1') then
+				--s_nextState <= TStop;
+				--s_isE <= '1';
+			--else
+			if (timeExp = '1' and s_isP1 = '0') then
+				s_nextstate <= TTirarAgua;
+				s_ultimoestado <= '1';
+				s_ultimoestado_1 <= '0';
+			elsif (timeExp = '1' and s_ultimoestado_1 ='1' and s_ultimoestado_2 = '1') then
 				s_nextstate <= TTirarAgua;
 				s_ultimoestado_1 <= '0';
-			elsif (timeExp = '1' and startStop = '1') then
+			elsif (timeExp = '1') then
 				s_nextstate <= TTirarAgua;
 				s_ultimoestado <= '1';
 				s_ultimoestado_1 <= '1';
@@ -168,10 +290,11 @@ architecture Behavioral of P1TesteFSM is
 			else
 				s_nextState <= TEnxaguar;
 			end if;
+			--end if;
 --------------------------------------------------------------------
 		when TFinish =>
 			display1 <= '1';
-			display2 <= "1111001";
+			display2 <= "1111111";
 			water_pump <= '0';
 			spin <= '0';
 			on_off <= '1';
@@ -186,7 +309,3 @@ architecture Behavioral of P1TesteFSM is
 		end case;
 	end process;
 end Behavioral;
-		
-
-		
-	
